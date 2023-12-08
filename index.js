25ac5b03094acec4d655d9cd455dbee276a8ec9a
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-12-09 02:08:49 +0530

    client whitelisted

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const { Server } = require("socket.io");
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: 'https://localhost:3000' }))
+const isLocal = app.settings.env === 'local';
+const URL = isLocal ? 'https://localhost:3000' : "https://draw-io-arghya.vercel.app";
+app.use(cors({ origin: URL }))
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: 'https://localhost:3000' });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
-    console.log("connected");
+    console.log("server connected");
+
+    socket.on('beginPath', (arg) => {
+        socket.broadcast.emit('beginPath', arg)
+    });
+
+    socket.on('drawLine', (arg) => {
+        socket.broadcast.emit('drawLine', arg)
+    });
+
+    socket.on('changeConfig', (arg) => {
+        socket.broadcast.emit('changeConfig', arg)
+    });
+
+    socket.on('changeActiveItem', (arg) => {
+        socket.broadcast.emit('changeActiveItem', arg)
+    });
 });
 
-httpServer.listen(5000);
+httpServer.listen(5001);
